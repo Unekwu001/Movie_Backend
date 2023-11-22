781a95ed24bae84fce57ec32cdeb904d71c9e79d
Theophilus Unekwu <unekwutheophilus@gmail.com>
2023-11-22 13:10:55 +0100

    Add project files.

---
+++ movieProject/Controllers/MovieController.cs
@@ -0,0 +1,50 @@
+﻿using Data.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace movieProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+
+
+        private static List<string> searchHistory = new List<string>();
+        private readonly string apiKey = "f51e7b79"; // Get your API key from http://www.omdbapi.com
+
+
+        [HttpGet("search/{title}")]
+        public async Task<IActionResult> SearchMovie(string title)
+        {
+            // Save search query to history
+            searchHistory.Insert(0, title);
+            if (searchHistory.Count > 5)
+                searchHistory.RemoveAt(5);
+
+            // Call OMDB API
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync($"http://www.omdbapi.com/?apikey={apiKey}&t={title}");
+                var result = JsonConvert.DeserializeObject<MovieDetails>(response);
+                return Ok(ApiResponse.Success(result));
+            }
+        }
+
+
+
+
+
+        [HttpGet("searchHistory")]
+        public IActionResult GetSearchHistory()
+        {
+            return Ok(ApiResponse.Success(searchHistory.Take(5)));
+        }
+
+
+
+
+
+    }
+}
---
+++ movieProject/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+ 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://movie-fe-rose.vercel.app") // Replace with your React app's URL
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .WithExposedHeaders("Authorization"); // This adds the custom authorization header to response
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
